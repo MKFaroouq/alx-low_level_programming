5dc2fd9954a691dda625c2fc599446ca6cf4b280
MKFaroouq <IproBazooka@gmail.com>
2023-07-18 18:36:43 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *add - Entry point
+ *@x:first_number
+ *@y:second_number
+ *Description: function that adds two integers and returns the result.
+ *Return: x + y
+*/
+
+int add(int x, int y)
+{
+	return (x + y);
+}
