7686c19351bf5edf0d73e548bdc1ae46396f1a2e
MKFaroouq <IproBazooka@gmail.com>
2023-07-21 18:13:13 -0700

    0x04-more_functions_nested_loops #adv

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -5,25 +5,21 @@
  *
  * @n: The ineger to be printed.
  *
- * Return:Always 0
+ *Return:Always 0
  */
 
 void print_number (int n )
 {
 	unsigned int i = n;
 
-	/*first check if its negative*/
 	if (n < 0) 
 	{
 		_putchar(45);
 		i = -i ;
 	}
-	
-	/*print the first few digits*/
 	if ( i / 10 )
 	{
 		print_number(i / 10);
 	}
-	/*print the last digits*/
 	_putchar(i % 10 + '0')'
 }	
