453bcd0be92f3b7f5685c0acad334322a33e33ba
MKFaroouq <IproBazooka@gmail.com>
2023-07-21 18:00:51 -0700

    0x04-more_functions_nested_loops #adv

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -20,7 +20,7 @@ void print_number (int n )
 	
 	/*print the first few digits*/
 	if ((num / 10) > 0 )
-		print_number(num/10);
+		print_number(num / 10);
 
 	/*print the last digits*/
 	_putchar((num % 10 ) + '0');
